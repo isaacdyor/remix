16d665cfad6a494b840edb86271c03d2def852ce
Isaac Dyor <isaac@dyor.com>
2026-05-23 17:38:24 -0700

    fix refreshing

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,9 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withUniwindConfig } = require("uniwind/metro");
-const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro-config");
+const {
+  wrapWithReanimatedMetroConfig,
+} = require("react-native-reanimated/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(process.cwd());
+const alchemyRuntimeDataPattern = /packages[/\\]infra[/\\]\.alchemy[/\\].*/;
+
+config.resolver.blockList = [
+  ...(Array.isArray(config.resolver.blockList)
+    ? config.resolver.blockList
+    : [config.resolver.blockList].filter(Boolean)),
+  alchemyRuntimeDataPattern,
+];
 
 const uniwindConfig = withUniwindConfig(wrapWithReanimatedMetroConfig(config), {
   cssEntryFile: "./global.css",
